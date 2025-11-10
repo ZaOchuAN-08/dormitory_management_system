19a06ca334a7101796f7076c2f33d9672709478e
ZaOchuAN-08 <123090837@link.cuhk.edu.cn>
2025-11-10 21:41:21 +0800

    update eighth

--- backend/sql_data_fetch/sql_query.sql
+++ backend/sql_data_fetch/sql_query.sql
@@ -1,14 +1,20 @@
 SELECT `STUDENT_ID`, `PASSWORD`
 FROM student
-WHERE room_id = 'C502'; --可更改为想要的房间
+WHERE room_id = 'B414'; --可更改为想要的房间
 
 SELECT t.`TUTOR_ID`, t.`TUTOR_PASSWORD`
 FROM tutor AS t
 JOIN floor AS f ON t.`TUTOR_ID` = f.`TUTOR_ID`
-WHERE f.`BUILDING_ID` = 'C'
-  AND f.`FLOOR_ID` = '5'; --可更改为想要的楼栋和楼层
+WHERE f.`BUILDING_ID` = 'B'
+  AND f.`FLOOR_ID` = '4'; --可更改为想要的楼栋和楼层
 
 SELECT w.`WARDEN_ID`, w.`WARDEN_PASSWORD`
 FROM warden AS w
 JOIN building AS b ON w.`WARDEN_ID` = b.`WARDEN_ID`
-WHERE b.`BUILDING_ID` = 'C';  --可更改为想要的楼栋
+WHERE b.`BUILDING_ID` = 'B';  --可更改为想要的楼栋
+
+SELECT * FROM student WHERE `STUDENT_ID`=;
+
+SELECT * FROM tutor WHERE `TUTOR_ID`=;
+
+SELECT * FROM warden WHERE `WARDEN_ID`=;
